db4df5e632d69cf89cd0d25898f3fd5021af97c4
pavin-raj <pavinrajofficial@gmail.com>
2025-10-27 23:55:39 +0530

    feat(infra): initialize AWS CDK TypeScript setup
    Introduces a new 'infra' directory with AWS CDK project structure including:
    - Stack definition for a DynamoDB table
    - Configuration and test setup
    - Supporting .gitignore and .npmignore files
    Also updates the root package.json to include the infra project.

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const table = new dynamodb.Table(this, 'RankingsTable', {
+      tableName: 'rankings-table',
+      partitionKey: {
+        name: 'rankingId',
+        type: dynamodb.AttributeType.STRING
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
